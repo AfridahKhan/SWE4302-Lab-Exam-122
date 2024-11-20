e42bd26ddd7a34ee481955532c9235c8cd7ecc26
AfridahKhan <afridahkhan@gmail.com>
2024-11-20 16:15:11 +0600

    Add files via upload

---
+++ IceCreamFlavor.java
@@ -0,0 +1,10 @@
+public class IceCreamFlavor {
+    public double pricePerScoop;
+    String name;
+
+    public IceCreamFlavor( String name,double pricePerScoop) {
+
+        this.name = name;
+        this.pricePerScoop = pricePerScoop;
+    }
+}
