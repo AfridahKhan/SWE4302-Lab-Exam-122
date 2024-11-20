0f05373e3f2f278fcc2793969a3c917e4b153a25
AfridahKhan <afridahkhan@gmail.com>
2024-11-20 16:15:58 +0600

    Add files via upload

---
+++ IceCreamTopping.java
@@ -0,0 +1,10 @@
+public class IceCreamTopping {
+    String name;
+    double price;
+
+    IceCreamTopping(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
